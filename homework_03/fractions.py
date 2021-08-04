68b1d3df3c80a40dad707bf4a8cc0693bdff6259
RebecaDumitrascuta <rebecadumitrascuta02@gmail.com>
2021-08-04 19:35:55 +0300

    fractions in progress

---
+++ homework_03/fractions.py
@@ -0,0 +1,37 @@
+class Fraction:
+    def __init__(self, counter, denominator):
+        print ('This is the constructor of Fraction Class')
+        self.counter = counter
+        self.denominator = denominator
+    # def __str__(self):
+    #     return f'Object of type{type(self)} with counter = '{self.counter}'
+    #     print(counter, '/', denominator)
+
+    @property
+    def counter(self):
+        return self._counter
+
+
+    @counter.setter
+    def counter(self, counter):
+        self._counter = counter
+
+    @counter.deleter
+    def counter(self):
+        self._counter
+
+    @property
+    def denominator(self):
+        return self._denominator
+
+    @denominator.setter
+    def counter(self, denominator):
+        self.denominator = denominator
+
+    @denominator.deleter
+    def counter(self):
+        self._denominator
+
+if __name__ == '__main__':
+    counter = Fraction(counter)
+    print(counter)
