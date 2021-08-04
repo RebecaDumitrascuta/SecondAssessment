18fc9b1bf9d54d175e6c7b6197e12e06e051ce6d
RebecaDumitrascuta <rebecadumitrascuta02@gmail.com>
2021-08-04 22:04:35 +0300

    homework 3 ready to be checked

--- homework_03/fractions.py
+++ homework_03/fractions.py
@@ -6,40 +6,26 @@
 # ○ inverse: returnează o nouă fracție (inversa fracției)
 
 class Fraction:
-    def __init__(self, counter, denominator):
-
-        print ('This is the constructor of Fraction Class')
-        self.counter = counter
-        self.denominator = denominator
-    # def __str__(self):
-    #     return f'Object of type{type(self)} with counter = '{self.counter}'
-    #     print(counter, '/', denominator)
-
-    @property
-    def counter(self):
-        return self._counter
-
-
-    @counter.setter
-    def counter(self, counter):
-        self._counter = counter
-
-    @counter.deleter
-    def counter(self):
-        self._counter
-
-    @property
-    def denominator(self):
-        return self._denominator
-
-    @denominator.setter
-    def counter(self, denominator):
+    def __init__(self, cont, denominator):
+        self.cont = cont
         self.denominator = denominator
 
-    @denominator.deleter
-    def counter(self):
-        self._denominator
+    def __str__(self):
+        return 'This is ' + str(self.cont) + '/' + str(self.denominator)
+
+    def __add__(self):
+        return self.cont + self.denominator
+
+    def __sub__(self):
+        return self.cont - self.denominator
+
+    def inverse(self):
+        return self.denominator / self.cont
+
 
 if __name__ == '__main__':
-    counter = Fraction(counter)
-    print(counter)
+    obj_01 = Fraction(cont=10, denominator=2)
+    print(obj_01)
+    print(obj_01.__add__())
+    print(obj_01.__sub__())
+    print(obj_01.inverse())
